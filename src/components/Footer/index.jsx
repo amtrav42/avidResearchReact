d143cac13475c2c858038fe7c0d2880cab86d58c
amaltr42 <amelia.travers@gmail.com>
2019-09-26 12:41:06 +1000

    added copyright year js in footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,8 +2,14 @@ import React, { Component } from "react";
 
 class Footer extends Component {
   render(){
+    const d = new Date();
+    const n = d.getFullYear();
     return (
-      <footer>This is our footer, 100% footer</footer>
+      <footer>
+        {`Thank you for visiting Avid Research where we like to ask all the questions and occasionally even answer them.
+        Feel free to reference any of the images here (please link people back to this site!) and contact us if you would like your very own commission.
+        Copyright ${n}`}
+      </footer>
     );
   }
 }
