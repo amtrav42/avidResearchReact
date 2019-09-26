ec6c773021491deaf82c95ff5fef0359e9f2020f
amaltr42 <amelia.travers@gmail.com>
2019-09-26 17:14:26 +1000

    added nav component

---
+++ src/components/Nav/index.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Nav extends Component {
+  render(){
+    return (
+      <nav>
+        This is our nav where all the links will be
+      </nav>
+    );
+  }
+}
+export default Nav;
--- src/components/Template/index.jsx
+++ src/components/Template/index.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Footer from "../Footer";
 import Header from "../Header";
 import Main from "../Main";
+import Nav from "../Nav";
 
 //Declare a class called Template, it's a prototype of the base react component//
 class Template extends Component {
@@ -9,6 +10,7 @@ class Template extends Component {
     return (
       <div>
         <Header/>
+        <Nav/>
         <Main/>
         <Footer/>
       </div>
