1b8fe66b13085be6f62b5e90ce2be9290b099b07
amaltr42 <amelia.travers@gmail.com>
2019-10-06 22:22:56 +1100

    added sass

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
+import './styles.scss';
 
 class Header extends Component {
   render(){
     return (
       <header>
-        <h1>Welcome to Avid Research</h1>
+        <h1 className='header'>Welcome to Avid Research</h1>
       </header>
     );
   }
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,12 @@ const path = require('path');
 const BUILD_DIR = path.resolve(__dirname, 'public');
 const APP_DIR = path.resolve(__dirname, 'src');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const MiniCssExtractPluginConfig = new MiniCssExtractPlugin({
+  filename: '[name].css',
+  chunkFilename: '[id].css',
+})
 
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
   template: `${APP_DIR}/index.html`,
@@ -31,9 +37,22 @@ module.exports = {
         use: ['babel-loader'],
         exclude: /node_modules/,
       },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          //translates css into Common JS//
+          'css-loader',
+          //compiles sass into css//
+          'sass-loader',
+        ],
+      },
     ],
   },
   plugins: [
     HtmlWebpackPluginConfig,
+    MiniCssExtractPluginConfig,
   ],
 };
